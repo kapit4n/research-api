2fff7cc0c30e52d9b6976a79a9c70627f86665da
lartl <luis.arce22@gmail.com>
2018-12-02 08:25:35 -0400

    Added status field name to Research and ResearchGoal

--- db/db_backup.sql
+++ db/db_backup.sql
@@ -25,3 +25,203 @@
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2018-11-30 21:31:35
+
+DROP TABLE IF EXISTS `accesstoken`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accesstoken` (
+  `id` varchar(255) NOT NULL,
+  `ttl` int(11) DEFAULT NULL,
+  `scopes` text,
+  `created` datetime DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accesstoken`
+--
+
+LOCK TABLES `accesstoken` WRITE;
+/*!40000 ALTER TABLE `accesstoken` DISABLE KEYS */;
+/*!40000 ALTER TABLE `accesstoken` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `acl`
+--
+
+DROP TABLE IF EXISTS `acl`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `acl` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `model` varchar(512) DEFAULT NULL,
+  `property` varchar(512) DEFAULT NULL,
+  `accessType` varchar(512) DEFAULT NULL,
+  `permission` varchar(512) DEFAULT NULL,
+  `principalType` varchar(512) DEFAULT NULL,
+  `principalId` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `acl`
+--
+
+LOCK TABLES `acl` WRITE;
+/*!40000 ALTER TABLE `acl` DISABLE KEYS */;
+/*!40000 ALTER TABLE `acl` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `research`
+--
+
+DROP TABLE IF EXISTS `research`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `research` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(512) NOT NULL,
+  `status` varchar(512) DEFAULT NULL,
+  `startDate` datetime DEFAULT NULL,
+  `endDate` datetime DEFAULT NULL,
+  `description` varchar(512) DEFAULT NULL,
+  `imageUrl` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `research`
+--
+
+LOCK TABLES `research` WRITE;
+/*!40000 ALTER TABLE `research` DISABLE KEYS */;
+INSERT INTO `research` VALUES (2,'Requirements Project','new','2018-06-25 16:22:58','2018-09-25 16:22:58','This project is to discover the requirements of the requirements project.','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsrKpPLycp-oOHz7HLj6Uu41Vz3NDADmC1rryuj2KfaMDGaiLp'),
+(3,'Angular Material','new','2018-06-25 16:22:58','2018-09-25 16:22:58','This is a project to display the goals that we should reach in a angular material research','http://2.bp.blogspot.com/-9e_dXZnEGu0/Vm-LQcOazUI/AAAAAAAAEtU/rbSXZLBzmtw/s1600/material-big.png');
+/*!40000 ALTER TABLE `research` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `researchgoal`
+--
+
+DROP TABLE IF EXISTS `researchgoal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `researchgoal` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(512) DEFAULT NULL,
+  `status` varchar(512) DEFAULT NULL,
+  `description` varchar(512) DEFAULT NULL,
+  `imageUrl` varchar(512) DEFAULT NULL,
+  `researchId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `researchgoal`
+--
+
+LOCK TABLES `researchgoal` WRITE;
+/*!40000 ALTER TABLE `researchgoal` DISABLE KEYS */;
+INSERT INTO `researchgoal` VALUES 
+(1,'Register a project','new','This is something that I need to do first','http://cdn2.itpro.co.uk/sites/itpro/files/styles/article_main_wide_image/public/2017/04/project_management_0.jpg?itok=8Uny5SVW',2);
+/*!40000 ALTER TABLE `researchgoal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(512) NOT NULL,
+  `description` varchar(512) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rolemapping`
+--
+
+DROP TABLE IF EXISTS `rolemapping`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rolemapping` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `principalType` varchar(512) DEFAULT NULL,
+  `principalId` varchar(255) DEFAULT NULL,
+  `roleId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `principalId` (`principalId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rolemapping`
+--
+
+LOCK TABLES `rolemapping` WRITE;
+/*!40000 ALTER TABLE `rolemapping` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rolemapping` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `realm` varchar(512) DEFAULT NULL,
+  `username` varchar(512) DEFAULT NULL,
+  `password` varchar(512) NOT NULL,
+  `email` varchar(512) NOT NULL,
+  `emailVerified` tinyint(1) DEFAULT NULL,
+  `verificationToken` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-07-18 16:44:56
